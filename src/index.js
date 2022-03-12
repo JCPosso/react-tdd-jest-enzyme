6187b37e4265fd4c32d71a4068eead7d1b4a359f
JCPosso <juancamiloposso01@gmail.com>
2022-03-12 18:29:39 -0500

    add PWA

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from './serviceWorkerRegistration';
 
 window.$token = "";
 window.$expirationDate = "";
@@ -13,3 +14,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+serviceWorker.register()
