b853228339da2cc5f2f03e823169def69d2ccaaa
JCPosso <juancamiloposso01@gmail.com>
2022-03-31 23:05:28 -0500

    Add mock use navigate

--- src/pages/login/index.test.js
+++ src/pages/login/index.test.js
@@ -1,8 +1,18 @@
 import { shallow } from "enzyme";
+import { ThemeContext } from "../../ThemeContext";
 import Login from "./index";
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate
+}));
 
 describe("<Login/>", () => {
   it("should render correctly", () => {
+    jest.spyOn(Login, 'useAppContext')
+			.mockImplementation(() => ThemeContext);
     const wrapper = shallow(<Login />);
     expect(wrapper).toHaveLength(1);
   });
